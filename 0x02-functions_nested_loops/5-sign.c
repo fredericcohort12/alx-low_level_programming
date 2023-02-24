2037f4e74b2848c2a67e9b218ad3f1e0b8889bf4
fredericcohort12 <fredericdanimbe22@gmail.com>
2023-02-24 00:22:22 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,12 @@
  *0 if n is zero
  *-1 and prints - if n is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	}
-	else if (n == 0)
+	}else if (n == 0)
 }
