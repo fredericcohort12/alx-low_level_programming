ecb789b22b543504bd264da5242171d6a96f9cc8
fredericcohort12 <fredericdanimbe22@gmail.com>
2023-02-23 18:31:12 -0800

    all the files

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  * Return On succes 1
  * On error, -1 is returned, and errno is set appropriately
  */
-int_putchar(char c)
+int _putchar(char c)
 {
-        return(write(1,&c,1));
+        return(write(1, &c, 1));
 }
