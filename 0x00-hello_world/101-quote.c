9eee503db31ddf716df7dd6f917f98e186c6de84
fredericcohort12 <fredericdanimbe22@gmail.com>
2023-02-26 11:05:09 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	print("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
